45af6d2ce4e7b5b2a6627f49bb8778af1f40fb3b
Ruthvik-1411 <nagasairuthvik1919@gmail.com>
2024-10-19 12:17:42 +0530

    chore: initial changes for srpass v2

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/*
+Created by Ruthvik on 10/06/2023
+Part of a code for SRpass app in react native
+*/
+import {NavigationContainer} from "@react-navigation/native";
+import BottomTab from "./BottomTab";
+const App=()=> {
 
-export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <BottomTab/>
+    </NavigationContainer>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  );
+};
+
+export default App;
